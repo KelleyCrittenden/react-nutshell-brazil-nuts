31bef0511233d05984c028dd163bf9294c1006d5
Brett Stoudt <Bstoudt01@gmail.com>
2020-07-17 10:39:06 -0500

    is something broken?

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,19 @@
+import { Route } from "react-router-dom";
+import React from "react";
+import MessageCard from "./message/MessageCard";
+
+const ApplicationViews = () => {
+    return (
+		<React.Fragment>
+
+			<Route
+				path="/"
+				render={() => {
+					return <> <MessageCard /> </>
+				}}/>
+            
+        </React.Fragment>
+	);
+};
+
+export default ApplicationViews;
---
+++ src/components/Nutshell.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ApplicationViews from "./ApplicationViews";
+import "./Nutshell.css";
+
+
+const Nutshell = () => {
+return (
+    <>
+      <ApplicationViews />
+    </>
+  );
+};
+
+export default Nutshell;
